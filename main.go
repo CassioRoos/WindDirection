b3ad96fe97c011c174312b5f2a07a7372d228c70
Cassio Roos <cassio.roos@dextra-sw.com>
2020-10-13 16:35:15 -0300

    with chan pipeline

--- main.go
+++ main.go
@@ -11,7 +11,6 @@ import (
 	"time"
 )
 
-
 var (
 	windRegex     = regexp.MustCompile(`\d* METAR.*EGLL \d*Z [A-Z ]*(\d{5}KT|VRB\d{2}KT).*=`)
 	tafValidation = regexp.MustCompile(`.*TAF.*`)
@@ -23,72 +22,96 @@ var (
 	windDist      [8]int
 )
 
-func parseToArray(text string) []string{
-	lines := strings.Split(text, "\n")
-	metarSlice := make([]string,0, len(lines))
-	metarStr := ""
-	for _, line := range  lines{
-		if tafValidation.MatchString(line){
-			break
-		}
-		if !comment.MatchString(line){
-			metarStr += strings.Trim(line, " ")
-		}
-		if metarClose.MatchString(line){
-			metarSlice = append(metarSlice, metarStr)
-			metarStr = ""
-		}
-	}
-	return metarSlice
-}
-
-func extractWindDirection(metars []string) []string{
-	winds := make([]string, 0, len(metars))
-	for _, metar := range metars{
-		if windRegex.MatchString(metar){
-			winds = append(winds, windRegex.FindAllStringSubmatch(metar,-1)[0][1])
-		}
-	}
-	return winds
-}
-
-func mineWindDistribution(winds []string){
-	for _, wind := range winds{
-		if variableWind.MatchString(wind){
-			for i := 0; i< 8; i++{
-				windDist[i]++
+func parseToArray(textChannel chan string, metarChannel chan []string) {
+	for text := range textChannel {
+		lines := strings.Split(text, "\n")
+		metarSlice := make([]string, 0, len(lines))
+		metarStr := ""
+		for _, line := range lines {
+			if tafValidation.MatchString(line) {
+				break
 			}
-		}else if validWind.MatchString(wind){
-			windStr := windDirOnly.FindAllStringSubmatch(wind, -1)[0][1]
-			if d, err := strconv.ParseFloat(windStr, 64); err == nil{
-				dirIndex := int(math.Round(d/45.0))%8
-				windDist[dirIndex]++
+			if !comment.MatchString(line) {
+				metarStr += strings.Trim(line, " ")
+			}
+			if metarClose.MatchString(line) {
+				metarSlice = append(metarSlice, metarStr)
+				metarStr = ""
 			}
 		}
+		metarChannel <- metarSlice
 	}
+	//return metarSlice
+	close(metarChannel)
 }
 
-func main()  {
+func extractWindDirection(metarChannel, windChannel chan []string) {
+	for metars := range metarChannel {
+		winds := make([]string, 0, len(metars))
+		for _, metar := range metars {
+			if windRegex.MatchString(metar) {
+				winds = append(winds, windRegex.FindAllStringSubmatch(metar, -1)[0][1])
+			}
+		}
+		windChannel <- winds
+	}
+	close(windChannel)
+}
+
+func mineWindDistribution(windsChannel chan []string, distChannel chan [8]int) {
+	for winds := range windsChannel {
+		for _, wind := range winds {
+			if variableWind.MatchString(wind) {
+				for i := 0; i < 8; i++ {
+					windDist[i]++
+				}
+			} else if validWind.MatchString(wind) {
+				windStr := windDirOnly.FindAllStringSubmatch(wind, -1)[0][1]
+				if d, err := strconv.ParseFloat(windStr, 64); err == nil {
+					dirIndex := int(math.Round(d/45.0)) % 8
+					windDist[dirIndex]++
+				}
+			}
+		}
+	}
+	distChannel <- windDist
+	close(distChannel)
+
+}
+
+func main() {
+	textChannel := make(chan string)
+	metarChannel := make(chan []string)
+	windsChannel := make(chan []string)
+	resultChannel := make(chan [8]int)
+
+	// 1. Change to array, each metar report is a separate item in the array
+	go parseToArray(textChannel, metarChannel)
+
+	// 2. Extract wind direction, EGLL 312350X 07004KT CAVOK 12/09 Q1016 NOSIG= -> 070
+	go extractWindDirection(metarChannel, windsChannel)
+
+	// 3. Assign to N, NE, E, SE, S, SW, W, NW, 070 -> E + 1
+	go mineWindDistribution(windsChannel, resultChannel)
+
 	absPath, _ := filepath.Abs("./metarfiles/")
 	files, _ := ioutil.ReadDir(absPath)
 	start := time.Now()
-	for _, file := range files{
+	for _, file := range files {
 		dat, err := ioutil.ReadFile(filepath.Join(absPath, file.Name()))
-		if err != nil{
+		if err != nil {
 			panic(err)
 		}
 		text := string(dat)
-
-		// 1. Change to array, each metar report is a separate item in the array
-		metarsReports := parseToArray(text)
-
-		// 2. Extract wind direction, EGLL 312350X 07004KT CAVOK 12/09 Q1016 NOSIG= -> 070
-		windsDirections := extractWindDirection(metarsReports)
-
-		// 3. Assign to N, NE, E, SE, S, SW, W, NW, 070 -> E + 1
-		mineWindDistribution(windsDirections)
+		textChannel <- text
 	}
+	close(textChannel)
+	results := <-resultChannel
 	elapsed := time.Since(start)
-	fmt.Printf("%v\n", windDist)
+	fmt.Printf("%v\n", results)
 	fmt.Printf("Processing took %s\n", elapsed)
+	//[32003 34077 32959 23334 38005 54242 57604 28738]
+	//[32003 34077 32959 23334 38005 54242 57604 28738]
+	//Processing took 3.241896604s
+
 }
